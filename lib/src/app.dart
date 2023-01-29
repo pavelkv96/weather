5fb765204f44277c554f46b7909b1010c8f5a5b3
Pavel Kalinin <pavelkv96@gmail.com>
2023-01-29 13:43:34 +0300

    feat: add selected city in root application

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:weather/generated/locale_keys.g.dart';
 import 'package:weather/src/di/injection.dart';
+import 'package:weather/src/feature/selected_city/selected_city.dart';
 import 'package:weather/src/navigation/navigation.dart';
 
 class App extends StatelessWidget {
@@ -29,29 +30,48 @@ class _App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider<SelectedCityBloc>(create: (context) => locator<SelectedCityBloc>()),
         BlocProvider<NavigationBloc>(create: (context) => locator<NavigationBloc>()),
       ],
-      child: ChangeNotifierProvider<AppRouter>(
-        create: (context) => locator<AppRouter>(),
-        child: BlocBuilder<NavigationBloc, NavigationState>(
-          builder: (context, state) {
-            final appRouter = context.read<AppRouter>();
-            return MaterialApp.router(
-              routerDelegate: AutoRouterDelegate.declarative(
-                appRouter,
-                navRestorationScopeId: 'root_auto_router',
-                routes: (_) => state.pages,
-              ),
-              routeInformationParser: appRouter.defaultRouteParser(includePrefixMatches: true),
-              restorationScopeId: 'root_router',
-              locale: context.locale,
-              supportedLocales: context.supportedLocales,
-              localizationsDelegates: context.localizationDelegates,
-              debugShowCheckedModeBanner: false,
-              onGenerateTitle: (context) => tr(LocaleKeys.core_application_title),
-              theme: ThemeData(primarySwatch: Colors.blue),
-            );
-          },
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<SelectedCityBloc, SelectedCityState>(
+            listenWhen: (previous, current) => previous != current,
+            listener: (context, state) {
+              final event = state.when(
+                unknown: () => const NavigationEvent.splash(),
+                unselected: () => const NavigationEvent.cities(),
+                selected: (city) {
+                  // todo check updates forecast for current city
+                  return const NavigationEvent.forecast();
+                },
+              );
+              context.read<NavigationBloc>().add(event);
+            },
+          ),
+        ],
+        child: ChangeNotifierProvider<AppRouter>(
+          create: (context) => locator<AppRouter>(),
+          child: BlocBuilder<NavigationBloc, NavigationState>(
+            builder: (context, state) {
+              final appRouter = context.read<AppRouter>();
+              return MaterialApp.router(
+                routerDelegate: AutoRouterDelegate.declarative(
+                  appRouter,
+                  navRestorationScopeId: 'root_auto_router',
+                  routes: (_) => state.pages,
+                ),
+                routeInformationParser: appRouter.defaultRouteParser(includePrefixMatches: true),
+                restorationScopeId: 'root_router',
+                locale: context.locale,
+                supportedLocales: context.supportedLocales,
+                localizationsDelegates: context.localizationDelegates,
+                debugShowCheckedModeBanner: false,
+                onGenerateTitle: (context) => tr(LocaleKeys.core_application_title),
+                theme: ThemeData(primarySwatch: Colors.blue),
+              );
+            },
+          ),
         ),
       ),
     );
