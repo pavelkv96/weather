118a5b842c113f7072d6cd428f984ba2d47932e5
Kalinin_PV <pavelkv96@gmail.com>
2023-01-31 11:42:32 +0300

    feat: add temperature string type, add datetime toStringFormat extension

--- lib/src/common/util/constants/temperature.dart
+++ lib/src/common/util/constants/temperature.dart
@@ -1,4 +1,8 @@
 enum Temperature {
-  celsius,
-  fahrenheit;
+  celsius(type: 'C'),
+  fahrenheit(type: 'F');
+
+  final String type;
+
+  const Temperature({required this.type});
 }
--- lib/src/common/util/extension/date_time.dart
+++ lib/src/common/util/extension/date_time.dart
@@ -1,3 +1,9 @@
+import 'package:intl/intl.dart';
+
 extension DateTimeExtension on DateTime {
   DateTime get today => DateTime(year, month, day);
+
+  String toStringFormat({String pattern = 'dd.MM.yyyy', String locale = 'en_US'}) {
+    return DateFormat(pattern, locale).format(this);
+  }
 }
